1ed4bcf3d25676d452e983119406ed27677940ca
rs-smsmt <ramin.saljoughi@icloud.com>
2024-05-12 14:59:45 +0330

    alpha 0.52

---
+++ End of Chapter Files/Chapter 06/my_website/blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Author, Post, Tag
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['caption']
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ['title', 'date', 'author']
+    list_filter = ('author', 'date', 'tags')
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
