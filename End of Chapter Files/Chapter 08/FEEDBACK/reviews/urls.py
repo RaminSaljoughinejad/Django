9623fe68c8506a7250fee7529d5683b285e956f5
rs-smsmt <ramin.saljoughi@icloud.com>
2024-05-20 13:17:12 +0330

    alpha 0.66

---
+++ End of Chapter Files/Chapter 08/FEEDBACK/reviews/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='reviews-root-url'),
+    path('results', views.ThankyouView.as_view(), name='reviews-result-url'),
+    path('reviews', views.ReviewsView.as_view(), name='all-reviews-url'),
+    path('reviews/<int:pk>', views.ReviewDetailView.as_view(), name='review-detail-url')
+]
